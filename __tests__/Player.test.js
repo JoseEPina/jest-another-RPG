6116369871551a6b36fad692f4ac59aff5236553
JoseEPina <jose.edpina@gmail.com>
2021-09-28 23:13:59 -0600

    Completed Player object creation with tests.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,5 @@
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
 
 test('creates a player object', () => {
    const player = new Player('Dave');
@@ -29,6 +30,25 @@ test('gets inventory from player or returns false', () => {
    expect(player.getInventory()).toEqual(false);
 });
 
+test('adds a potion to the inventory', () => {
+   const player = new Player('Dave');
+   const oldCount = player.inventory.length;
+
+   player.addPotion(new Potion());
+
+   expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+   const player = new Player('Dave');
+   player.inventory = [new Potion(), new Potion(), new Potion()];
+   const oldCount = player.inventory.length;
+
+   player.usePotion(1);
+
+   expect(player.inventory.length).toBeLessThan(oldCount);
+});
+
 test("gets player's health value", () => {
    const player = new Player('Dave');
 
@@ -56,4 +76,12 @@ test("subtracts from player's health", () => {
    player.reduceHealth(99999);
 
    expect(player.health).toBe(0);
+});
+
+test("gets player's attack value", () => {
+   const player = new Player('Dave');
+   player.strength = 10;
+
+   expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+   expect(player.getAttackValue()).toBeLessThanOrEqual(15);
 });
